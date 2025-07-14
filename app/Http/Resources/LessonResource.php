44f07f67ba7e44a39efdb2462fd30d67f5bee611
XoDeR <xoder.vs@gmail.com>
2025-07-14 16:14:50 +0200

    resources: lesson and quiz

--- app/Http/Resources/LessonResource.php
+++ app/Http/Resources/LessonResource.php
@@ -14,6 +14,18 @@ class LessonResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'video_url' => $this->video_url,
+            'logo_url' => $this->logo_url,
+            'control_color' => $this->control_color,
+            'quiz_id' => $this->quiz_id,
+            'quiz' => $this->whenLoaded('quiz', fn () => QuizResource::make($this->quiz)),
+            'thumbnail_url' => $this->thumbnail_url,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+        ];
     }
 }
--- app/Http/Resources/QuizResource.php
+++ app/Http/Resources/QuizResource.php
@@ -14,6 +14,12 @@ class QuizResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'questions' => $this->questions,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+        ];
     }
 }
