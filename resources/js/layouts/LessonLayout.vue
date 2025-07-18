91ef8b46ba15983d571b3dfbe4bf86494d20038f
XoDeR <xoder.vs@gmail.com>
2025-07-18 23:09:12 +0200

    lesson layout skeleton

---
+++ resources/js/layouts/LessonLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import LessonFooter from './Lesson/LessonFooter.vue';
+import LessonHeader from './Lesson/LessonHeader.vue';
+
+</script>
+<template>
+  <div class="min-h-screen flex flex-col bg-[#FDFDFC] text-[#1b1b18] dark:bg-[#0a0a0a] dark:text-[#EDEDEC]">
+    <LessonHeader>
+
+    </LessonHeader>
+    <LessonFooter>
+
+    </LessonFooter>
+
+  </div>
+</template>
