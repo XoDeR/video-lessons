078a1e49b0a82c8684f229f74725915c7acc95b6
XoDeR <xoder.vs@gmail.com>
2025-07-18 19:38:10 +0200

    lesson factory

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Quiz;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,23 @@ class LessonFactory extends Factory
      */
     public function definition(): array
     {
+        $videoUrls = [
+            'https://filesamples.com/samples/video/mp4/sample_960x540.mp4',
+        ];
+
+        $thumbnailUrls = [
+            'https://images.pexels.com/photos/33020130/pexels-photo-33020130.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=720&w=1280',
+            'https://images.pexels.com/photos/32742888/pexels-photo-32742888.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=720&w=1280',
+        ];
+
         return [
-            //
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'video_url' => $this->faker->randomElement($videoUrls),
+            'thumbnail_url' => $this->faker->randomElement($thumbnailUrls),
+            'logo_url' => 'https://picsum.photos/id/77/170/170',
+            'control_color' => $this->faker->hexColor,
+            'quiz_id' => Quiz::query()->first()->id,
         ];
     }
 }
