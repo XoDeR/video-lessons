c6ff0143517d45607a5a9992629dfb33ebafc10a
XoDeR <xoder.vs@gmail.com>
2025-07-20 17:23:57 +0200

    show all lessons as cards in guest home page

--- resources/js/pages/GuestHome/Index.vue
+++ resources/js/pages/GuestHome/Index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import LessonCard from '@/components/LessonCard.vue';
 import LessonLayout from '@/layouts/LessonLayout.vue';
+import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 
 const props = defineProps<{
@@ -9,14 +12,26 @@ const props = defineProps<{
     last_page: number
     next_page_url: string | null
   }
-}>()
+}>();
+
+const lessons = ref([...props.lessons.data])
 
 console.log(props);
 </script>
 
 <template>
+
+  <Head title="Lessons" />
+
   <div>Lessons</div>
   <LessonLayout>
-    <div>Lessons</div>
+    <template #title>Lessons</template>
+
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <Link v-for="lesson in lessons" :key="lesson.id" :href="route('lesson.show', lesson.id)" class="block">
+      <LessonCard :lesson="lesson" />
+      </Link>
+    </div>
+
   </LessonLayout>
 </template>
